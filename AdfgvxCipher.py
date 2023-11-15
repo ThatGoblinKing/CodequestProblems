69bf343e2fb84b2b16225b4073006dd378fdd507
ThatGoblinKing <gusjminecritter@gmail.com>
2023-11-15 10:39:17 -0700

    Advanced ADFGVXCipher

--- AdfgvxCipher.py
+++ AdfgvxCipher.py
@@ -1,11 +1,10 @@
 A = 0
-D = 2
-F = 3
-G = 4
-V = 5
-X = 6
+D = 1
+F = 2
+G = 3
+V = 4
+X = 5
 table = [[], [], [], [], [], []]
-transposeTable = []
 testCases = int(input())
 for i in range(testCases):
     for i in range(6):
@@ -13,17 +12,62 @@ for i in range(testCases):
     keyword = input()
     alphabeticKeyword = ''.join(sorted(keyword))
     encryptedMessage = input()
+    keywordLen = len(keyword)
+    rowCount = int(len(encryptedMessage) / keywordLen)
     transposeTable = [[], [], [], [], [], []]
+    alphabeticalTransposeTable = [[], [], [], [], [], []]
+    cypheredLetters = ''
+    hangingChars = ''
     for i in range(len(encryptedMessage)):
-        print(i, encryptedMessage[i])
-        if i%6 == 0:
-            columnNumber = 
-        elif i%6 == 1:
-        elif i%6 == 2:
-        elif i%6 == 3:
-        elif i%6 == 4:
+        columnNumber = i%keywordLen
+        if i < (keywordLen * rowCount) or keyword[i%keywordLen] == alphabeticKeyword[i%keywordLen]:
+            transposeTable[columnNumber].append(encryptedMessage[i])
         else:
-            columnNumber = 
-        transposeTable[columnNumber].append(encryptedMessage[i])
+            hangingChars += encryptedMessage[i]
+    hangingChars = hangingChars[::-1]
+    for i in range(len(hangingChars)):
+        transposeTable[alphabeticKeyword.find(keyword[i])].append(hangingChars[i])
+    for i in  range(keywordLen):
+        alphabeticalTransposeTable[keyword.find(alphabeticKeyword[i])] = transposeTable[i]
+    for i in range(rowCount + 1):
+        for j in range(len(transposeTable)):
+            if j%2 == 0:
+                cypheredLetters+= "_"
+            try:
+                cypheredLetters += alphabeticalTransposeTable[j][i]
+            except:
+                pass
+    letterPairs = cypheredLetters.split('_')
 
-    print(transposeTable)
+    finalOutput = ""
+    decyphered = ""
+    for pair in letterPairs:
+        if len(pair) == 2:
+            if pair[0] == 'A':
+                x = A
+            elif pair[0] == 'D':
+                x = D
+            elif pair[0] == 'F':
+                x = F
+            elif pair[0] == 'G':
+                x = G
+            elif pair[0] == 'V':
+                x = V
+            else:
+                x = X
+
+            if pair[1] == 'A':
+                y = A
+            elif pair[1] == 'D':
+                y = D
+            elif pair[1] == 'F':
+                y = F
+            elif pair[1] == 'G':
+                y = G
+            elif pair[1] == 'V':
+                y = V
+            else:
+                y = X
+            decyphered += table[x][y]
+    finalOutput += decyphered
+print(finalOutput)
