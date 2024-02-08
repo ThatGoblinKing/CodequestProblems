3db45941e8ecb88cfbc61b6b5b629a379c5704d4
ThatGoblinKing <gusjminecritter@gmail.com>
2024-02-08 10:36:36 -0700

    Added input for ADFGVX and attempted FillInTheBlanks

---
+++ FillInTheBlanks/FillInTheBlanks.py
@@ -0,0 +1,32 @@
+def replaceBracket(line, currentOutput = ""):
+    if "[" and "]" not in line:
+        return currentOutput + line + "\n" #return everything that comes before it, the current line, and a new line to seperate it
+    else:
+        firstBracket = line.index('[') 
+        secondBracket = line.index(']') + 1
+
+        value = data[line[firstBracket + 1:secondBracket - 1]]
+
+        return replaceBracket(line[secondBracket:], currentOutput + line[:firstBracket] + value)
+
+finalOutput = ""
+
+n_cases = int(input())
+
+for _ in range(n_cases):
+
+    line = input().rstrip()
+
+    dataLength, letterLength = [int(val) for val in line.split(" ")]
+
+    data = {}
+    for i in range(dataLength):
+        name, value = input().rstrip().split(": ")
+        data[name] = value
+
+    letter = [input() for i in range(letterLength)]
+
+    for line in letter:
+        finalOutput += replaceBracket(line)
+else:
+    print(finalOutput.rstrip())
