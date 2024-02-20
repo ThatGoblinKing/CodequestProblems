c793852562ae63b7527916d98f172288eba97398
ThatGoblinKing <gusjminecritter@gmail.com>
2024-02-20 10:31:30 -0700

    Tried to debug FreeUpDiskSpace

--- FreeUpDiskSpace/FreeUpDiskSpace.py
+++ FreeUpDiskSpace/FreeUpDiskSpace.py
@@ -1,6 +1,8 @@
 import datetime
 
+finalOutput = ""
 TODAY = datetime.date(2019, 4, 27)
+EXPECTED_OUTPUT = "file7.mp3 36031.250\nfile6.sh 7703.125\nfile3.mov 7218.750\nfile10.jpg 7055.500\nfile4.gif 3656.250\nfile2.exe 3468.750\nfile5.doc 2234.375"
 
 class File():
     def __init__(self, rawDate: str, rawTime: str, pm: bool, size: int, fileName: str):
@@ -12,7 +14,7 @@ class File():
         self.size = size
         self.fileName = fileName
         self.calculateGrade()
-        
+
     def calculateGrade(self) -> int:
         time = TODAY - self.dateTime
         day = time.days
@@ -20,33 +22,35 @@ class File():
         megabytes = (self.size/1000)
         self.grade = day * megabytes
 
+
 n_cases = int(input())
 for _ in range(n_cases):
     files = []
     line = input().rstrip()
     fileCount, driveSize = [float(val) for val in line.split(" ")]
     fileCount = int(fileCount)
-
     for i in range(fileCount):
         line = input().rstrip()
         date, time, dayHalf, size, file = [val for val in line.split(" ")]
         files.append(File(date, time, dayHalf == "PM", int(size), file))
-    
+
     files.sort(key=lambda x: x.grade, reverse=True)
-    
-    driveSizeKilobytes = driveSize * 1000 * 1000 
-    
+
+    driveSizeKilobytes = driveSize * 1000 * 1000
     eraseSize = driveSizeKilobytes/4
-    alreadyErased = 0
-    
+
+    alreadyErased = 0 
+
     erasedFiles = []
-    
+
     for file in files:
         if alreadyErased >= eraseSize:
             break
         else:
             erasedFiles.append(file)
             alreadyErased += file.size
-    
+
     for file in erasedFiles:
-        print(file.fileName)
+        finalOutput += (f"{file.fileName} {file.grade:.03f}\n")
+
+print(finalOutput.strip()) 
