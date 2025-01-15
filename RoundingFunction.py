ef54810b94b9d19715be00082b6e0f924d534470
ThatGoblinKing <gusjminecritter@gmail.com>
2025-01-15 10:33:54 -0700

    Updated RoundingFunction so it can be easily imported. Continued progress on HighlightOfMyDay.py

--- RoundingFunction.py
+++ RoundingFunction.py
@@ -1,4 +1,4 @@
-def RoundHalfAway(num, decimalPoints):
+def round_half_away(num, decimalPoints):
     decimal = 10.0**-(decimalPoints + 1) #makes a decimal you can use to multiply/divide (IE decimalPoints = 2, decimal = 0.01 )
     roundedNum = abs(int((num/decimal) + decimal/10)) #puts digits you want to keep on the left side of the decimal (IE 1.394 = 139)
     finalDigit = roundedNum%10 #Gets the ones place
@@ -7,9 +7,9 @@ def RoundHalfAway(num, decimalPoints):
     roundedNum *= decimal * (num/abs(num))#Multiplies Num back down to the right number of decimal points and makes it positive/negative
     return roundedNum
 
-num = float(input("Number: "))
-
-while num != 0:
-    decimal = int(input("Decimal: "))
-    print(f"Rounded: {RoundHalfAway(num, decimal):.0{decimal}f}")
-    num = float(input("Number: "))
+# num = float(input("Number: "))
+#
+# while num != 0:
+#     decimal = int(input("Decimal: "))
+#     print(f"Rounded: {RoundHalfAway(num, decimal):.0{decimal}f}")
+#     num = float(input("Number: "))
