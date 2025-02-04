855eb158988dc726bab6b0a779b6aedb130d6deb
ThatGoblinKing <gusjminecritter@gmail.com>
2025-02-04 10:11:22 -0700

    Solved Null Cipher

---
+++ null-cipher/NullCipher.py
@@ -0,0 +1,12 @@
+#https://lmcodequestacademy.com/problem/null-cipher
+
+cases = int(input())
+for _ in range(cases):
+    line = input()
+    decyphered = ""
+    nextValidLetter = 0
+    for i in range(len(line) - 1):
+        if line[i] in "aeiou" and i >= nextValidLetter:
+            decyphered += line[i+1]
+            nextValidLetter = i + 2
+    print(decyphered)
